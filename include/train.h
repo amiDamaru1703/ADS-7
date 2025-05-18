5013cb3a0a403710f4089b22a0cbfd0d84ff0f38
amidamAru <ivanadrei18@gmail.com>
2025-05-18 20:26:55 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -12,12 +12,12 @@ private:
         Car* prev;
     };
     Car* first;
-    int64_t countOp;
+    int countOp;
 public:
     Train();
     ~Train();
     void addCar(bool light);
-    int64_t getLength();
-    int64_t getOpCount() const { return countOp; }
+    int getLength();
+    int getOpCount() const { return countOp; }
 };
 #endif  // INCLUDE_TRAIN_H_
