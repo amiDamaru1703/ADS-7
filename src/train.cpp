238042adf910f97db6cbaae09a215f6bcaf4a835
amidamAru <ivanadrei18@gmail.com>
2025-05-18 20:27:19 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,72 +1,74 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
-#include <cstdlib>
 
 Train::Train() : first(nullptr), countOp(0) {}
 
 Train::~Train() {
     if (!first) return;
-    Car* current = first;
-    do {
-        Car* temp = current;
-        current = current->next;
+    Car* cur = first->next;
+    while (cur != first) {
+        Car* temp = cur;
+        cur = cur->next;
         delete temp;
-    } while (current != first);
-}
-
-void Train::addCar(bool hasLight) {
-    Car* car = new Car(hasLight);
-    if (first == nullptr) {
-        car->prev = car;
-        car->next = car;
-        first = car;
-        return;
     }
-
-    car->next = first;
-    car->prev = first->prev;
-    first->prev->next = car;
-    first->prev = car;
+    delete first;
 }
 
-int64_t Train::getLength() {
+void Train::addCar(bool light) {
+    Car* newCar = new Car(light);
+    if (!first) {
+        first = newCar;
+        first->next = first;
+        first->prev = first;
+    }
+    else {
+        Car* last = first->prev;
+        last->next = newCar;
+        newCar->prev = last;
+        newCar->next = first;
+        first->prev = newCar;
+    }
+}
+
+int Train::getLength() {
     countOp = 0;
     if (!first) return 0;
-    Car* current = first;
-    bool hasIllumination = false;
-    while (true) {
-        if (current->light) {
-            hasIllumination = true;
+    const Car* ptr = first;
+    bool lightFound = false;
+    do {
+        if (ptr->light) {
+            lightFound = true;
             break;
         }
-        current = current->next;
-        if (current == first) break;
-    }
-    if (!hasIllumination) {
+        ptr = ptr->next;
+    } while (ptr != first);
+    if (!lightFound) {
         first->light = true;
-        Car* traveler = first->next;
-        int64_t counter = 1;
-        countOp++;
-        while (traveler != first) {
-            traveler = traveler->next;
-            counter++;
-            countOp++;
+        const Car* wal = first->next;
+        ++countOp;
+        int len = 1;
+        while (wal!= first) {
+            wal = wal->next;
+            ++countOp;
+            ++len;
         }
-        for (int64_t i = counter; i > 0; --i) {
-            traveler = traveler->prev;
-            countOp++;
+        for (int64_t i = 0; i < len; ++i) {
+            wal = wal->prev;
+            ++countOp;
         }
         first->light = false;
-        return counter;
-    } else {
-        Car* scanner = first;
-        int64_t total = 0;
-        do {
-            scanner = scanner->next;
-            total++;
-            countOp++;
-        } while (scanner != first);
-        countOp += total * total;
-        return total;
+        return len;
+    }
+    else {
+        const Car* wal = first->next;
+        ++countOp;
+        int64_t length = 1;
+        while (wal != first) {
+            wal = wal->next;
+            ++countOp;
+            ++length;
+        }
+        countOp += length * length;
+        return length;
     }
 }
