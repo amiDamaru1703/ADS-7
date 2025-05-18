32099e77a0c495db17f2e7768cbdb3f0ac4a4d46
amidamAru <ivanadrei18@gmail.com>
2025-05-18 17:40:13 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,15 @@
 // Copyright 2022 NNTU-CS
-#include "train.h"
 #include <iostream>
-#include <fstream>
-#include <chrono>
-#include <random>
-
-using namespace std;
-using namespace chrono;
-
-void runTest(const string& label, bool useRandom, bool lightState,
-    ofstream& timeOut, ofstream& opOut) {
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> dis(0, 1);
-    for (int n = 2; n <= 100; n += 2) {
-        Train train;
-        for (int i = 0; i < n; ++i) {
-            bool light = useRandom ? dis(gen) : lightState;
-            train.addCar(light);
-        }
-        auto start = high_resolution_clock::now();
-        int len = train.getLength();
-        auto end = high_resolution_clock::now();
-        auto duration = duration_cast<microseconds>(end - start).count();
-        int ops = train.getOpCount();
-        timeOut << label << "," << n << "," << duration << endl;
-        opOut << label << "," << n << "," << ops << endl;
-
-        cout << "[" << label << "] N=" << n << ", Len=" << len
-            << ", Ops=" << ops << ", Time=" << duration << "us" << endl;
-    }
-}
+#include "train.h"
 
 int main() {
-    ofstream timeOut("result/times.csv");
-    ofstream opOut("result/ops.csv");
-    runTest("off", false, false, timeOut, opOut);     // все лампочки выключены
-    runTest("on", false, true, timeOut, opOut);       // все включены
-    runTest("random", true, false, timeOut, opOut);   // случайные
-    timeOut.close();
-    opOut.close();
+    Train train;
+    int count = 60; // кол-во вагонов
+
+    while (count--)
+        train.addCar(false);
+
+    std::cout << train.getLength() << std::endl;
+    std::cout << train.getOpCount() << std::endl;
     return 0;
 }
